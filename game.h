1a5fe9bce55b9afaa2065722fcc5865543187e06
Miocaro-hub <1599182388@qq.com>
2021-04-16 21:55:07 +0800

    game.h

---
+++ game.h
@@ -0,0 +1,8 @@
+#pragma once
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+
+//º¯ÊıÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL],int row,int col);
